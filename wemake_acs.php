ef139c061a19bbf4f19fd3cc32c3579fe0c0782c
Ibrahim Shahine <ibra@wemake.co.il>
2025-01-10 12:56:12 +0200

    Change the plugin version

--- wemake_acs.php
+++ wemake_acs.php
@@ -8,7 +8,7 @@ Description: Wemake Accessibility plugin
 
 Plugin URI: http://wemake.co.il
 
-Version: 1.51
+Version: 1.53
 
 Author: Wemake Team
 
@@ -20,7 +20,7 @@ License: GPL2
 
 /*
 
-Copyright 2018  Wemake Team  (email : alex@wemake.co.il)
+Copyright 2025  wemake Team  (email : ibra@wemake.co.il)
 
 
 
@@ -60,7 +60,7 @@ define("WMACS_PLUGIN_NAME", 'Wemake Accessibility');
 
 define("WMACS_PLUGIN_SLUG", 'wemake-acs');
 
-define("WMACS_PLUGIN_VERSION", '1.51');
+define("WMACS_PLUGIN_VERSION", '1.53');
 
 define("WMACS_ABSPATH", dirname( __FILE__ ));
 
